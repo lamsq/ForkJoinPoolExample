d4b090a4d0a9b8b9303b8102ceeed30a019d9176
KIRILL SMIRNOV <kirill.smirnov@student.griffith.ie>
2024-11-05 13:05:28 +0300

    initial commit

---
+++ src/ForkJoinPoolExample.java
@@ -0,0 +1,31 @@
+import java.util.concurrent.*;
+
+class FactorialTask extends RecursiveTask<Long> {
+    private final int n;
+
+    public FactorialTask(int n) {
+        this.n = n;
+    }
+
+    @Override
+    protected Long compute() {
+        if (n <= 1)
+            return 1L;
+        FactorialTask subTask = new FactorialTask(n-1);
+        subTask.fork();
+        return n*subTask.join();
+    }
+}
+
+public class ForkJoinPoolExample {
+    public static void main(String[] args) {
+        int n = 10; // Вычисление факториала для числа 10
+
+        ForkJoinPool forkJoinPool = new ForkJoinPool();
+        FactorialTask factorialTask = new FactorialTask(n);
+
+        long result = forkJoinPool.invoke(factorialTask);
+
+        System.out.println("Факториал "+n+"! = "+result);
+    }
+}
